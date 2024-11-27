0f123eb1ad8f10540e6fd7c6e9932fee0961030b
Leon Prouger <leonprou@gmail.com>
2024-11-27 13:53:53 +0200

    adding create task script

---
+++ packages/contracts/scripts/create-task.ts
@@ -0,0 +1,39 @@
+
+import { TaskRegistry } from "../typechain-types";
+require('dotenv').config('../.env');
+
+const hre = require("hardhat");
+
+async function main() {
+    const taskRegistryAddress = process.env.TASK_REGISTRY_ADDRESS;
+    console.log("Task Registry Address:", taskRegistryAddress);
+    // Get the contract factory and deployer
+    const [deployer] = await hre.ethers.getSigners();
+
+    console.log("account address:", deployer.address);
+
+    // Get the deployed contract instance
+    const TaskRegistry = await hre.ethers.getContractFactory("TaskRegistry");
+    const taskRegistry = await TaskRegistry.attach(taskRegistryAddress) as TaskRegistry;
+
+    try {
+        // Create a task without ignition
+        const simpleTask = await taskRegistry.createTask(
+            "Do X for me",
+            0
+        );
+        // Wait for the transaction to be mined
+        const simpleTaskReceipt = await simpleTask.wait();
+        console.log("Simple task created in tx:", simpleTaskReceipt.hash);
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    }); 
