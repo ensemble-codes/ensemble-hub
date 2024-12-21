7baf1e1c48e0a45472376b1004f2d8d9eb755c55
yohann <yohann_88@hotmail.fr>
2024-12-21 20:56:00 +0200

    commit before checkout

--- packages/app/src/app/layout.tsx
+++ packages/app/src/app/layout.tsx
@@ -1,8 +1,11 @@
 import '../../global.css'
 import { Inter } from 'next/font/google'
 import Navigation from './components/Navigation'
+import PrivyWrapper from './components/PrivyWrapper'
+import LoginButton from './components/LoginButton'
 
 const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata = {
   title: 'Ensemble - Agentic Framework',
@@ -17,15 +20,18 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body className={`${inter.className} flex flex-col h-full`}>
-        <header className="bg-gray-800 text-white">
-          <div className="container mx-auto px-4 py-2 flex flex-col items-center">
-            <h1 className="text-2xl font-bold mb-2">Ensemble</h1>
-            <Navigation />
-          </div>
-        </header>
-        <main className="flex-grow bg-gray-100 overflow-hidden">
-          {children}
-        </main>
+          <PrivyWrapper>
+          <header className="bg-gray-800 text-white">
+            <div className="container mx-auto px-4 py-2 flex flex-col items-center">
+              <h1 className="text-2xl font-bold mb-2">Ensemble</h1>
+              <Navigation />
+              <LoginButton /> 
+            </div>
+          </header>
+          <main className="flex-grow bg-gray-100 overflow-hidden">
+            {children}
+          </main>
+        </PrivyWrapper>
       </body>
     </html>
   )
